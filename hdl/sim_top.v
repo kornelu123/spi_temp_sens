169d0899338605fb8c0d5f01efbfd24d1d49b0e4
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-10-09 18:09:38 +0200

    Register implementation

--- hdl/sim_top.v
+++ hdl/sim_top.v
@@ -1,6 +1,9 @@
-module sim_top();
+module sim_top;
+    reg clk;
+    reg reset;
+    reg [31:0] d_in;
+    wire [31:0] d_out;
 
-timer_tb timer_test();
-presc_tb presc_test();
-
+    timer_tb timer_test();
+    presc_tb presc_test();
 endmodule
